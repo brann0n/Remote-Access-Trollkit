bddf04015892a80cf0f8d9d2cde0ae34dc5a938a
Brandon <43607839+brann0n@users.noreply.github.com>
2020-05-25 13:56:33 +0200

    Did some coding to the client

---
+++ Trollkit Library/ClientDiscovery.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Trollkit_Library
+{
+	public class ClientDiscovery
+	{
+		private string BroadcastMessage { get; set; }
+		private string ExpectedReturnMessage { get; set; }
+		public ClientDiscovery(string broadcastMessage, string expectedReturnMessage)
+		{
+			BroadcastMessage = broadcastMessage;
+			ExpectedReturnMessage = expectedReturnMessage;
+		}
+
+		public UnicastIPAddressInformationCollection GetIpAddresses()
+		{
+			UnicastIPAddressInformationCollection BroadcastAddresses = null;
+
+			NetworkInterface[] Interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+			foreach (NetworkInterface Interface in Interfaces)
+			{
+				IPInterfaceProperties props = Interface.GetIPProperties();
+				if (props.GatewayAddresses.Count > 0 && Interface.OperationalStatus == OperationalStatus.Up)
+				{
+					BroadcastAddresses = props.UnicastAddresses; //0.0.0.255
+				}
+
+			}
+
+			return BroadcastAddresses;
+		}
+
+		public string GetRemoteServerIp(UnicastIPAddressInformationCollection addresses)
+		{
+			string ip = "";
+			string response = "";
+			while (true)
+			{
+				//check if the correct response has been received from the remote server, if true, abort the while loop.
+				if (response == ExpectedReturnMessage) break;
+
+				//setup the new udp client
+				UdpClient Client = new UdpClient();
+				Client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+				Client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+				//the data and server endpoint for later use
+				byte[] RequestData = Encoding.ASCII.GetBytes(BroadcastMessage);
+				IPEndPoint ServerEp = new IPEndPoint(IPAddress.Any, 0);
+
+				//loop through each unicast address and send a request to them
+				foreach (UnicastIPAddressInformation info in addresses)
+				{
+					Console.WriteLine($"Found Unicast address: {info.Address}");
+					if (info.Address.AddressFamily == AddressFamily.InterNetwork)
+					{
+						IPEndPoint local = new IPEndPoint(info.Address, 0);
+						Client.Client.Bind(local);
+						Client.Send(RequestData, RequestData.Length, new IPEndPoint(IPAddress.Broadcast, 9696));
+						Console.WriteLine($"Send data to {IPAddress.Broadcast.ToString()} address from {info.Address.ToString()}.");
+					}
+				}
+
+				//set the response timout to 5 seconds
+				Client.Client.ReceiveTimeout = 5000;
+				byte[] ServerResponseData;
+				try
+				{
+					//attempt to receive data from any ip as set by the Server Endpoint.
+					//throws an exception after 5 seconds if no response is received.
+					ServerResponseData = Client.Receive(ref ServerEp);
+					response = Encoding.ASCII.GetString(ServerResponseData);
+					Console.WriteLine($"Received {response} from {ServerEp.Address.ToString()}");
+					ip = ServerEp.Address.ToString();
+				}
+				catch (SocketException e)
+				{
+					if (e.SocketErrorCode == SocketError.TimedOut)
+					{
+						Console.WriteLine("Awaiting broadcast response timed out...");
+					}
+					else if (e.SocketErrorCode == SocketError.AddressAlreadyInUse)
+					{
+						//this error happens when the local port is in use, this either means the server is using it, or another program.
+						//attempt to connect to localhost anyway.
+						Console.WriteLine("Address was already in use, attempting to connect to localhost");
+						response = ExpectedReturnMessage;
+						ip = "127.0.0.1";
+					}
+					else
+					{
+						//if its something else the error should still be thrown.
+						throw e;
+					}
+				}
+
+				Client.Close();
+			}
+
+			return ip;
+		}
+	}
+}
---
+++ Trollkit Library/ClientReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trollkit_Library
+{
+	public class ClientReceiver
+	{
+
+		public void ConnectAndReceive(string ip)
+		{
+			Socket remoteSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			remoteSocket.Connect(new IPEndPoint(IPAddress.Parse(ip), 6969));
+			Console.WriteLine($"Connected to Trollkit host at {ip}:6969");
+			while (true)
+			{
+				//receive data from server.
+				DataTransferObject tObj = new DataTransferObject();
+			}
+		}
+	}
+}
