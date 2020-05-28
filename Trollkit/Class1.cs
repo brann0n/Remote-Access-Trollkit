ec8b4d4b57c513891550bdb69caa37668d7b34f7
jurriantanke <43601313+jurriantanke@users.noreply.github.com>
2020-05-28 11:19:24 +0200

    ui

--- Trollkit/Class1.cs
+++ Trollkit/Class1.cs
@@ -7,11 +7,14 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Drawing;
+using System.Windows.Input;
 
 namespace Trollkit
 {
     public class HeadMenuBtn : Button
     {
+        Boolean isActive = false;
+
         static HeadMenuBtn()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(HeadMenuBtn), new
@@ -20,7 +23,38 @@ namespace Trollkit
 
         protected override void OnClick()
         {
-            Console.WriteLine("moi"); 
+            Console.WriteLine("moi");
+            HeadMenu Main = (HeadMenu)Parent;
+            Main.setIsActiveToFalse();
         }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Canvas.SetLeft(this, 75);
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            Canvas.SetLeft(this, 30);
+        }
+    }
+
+    public class HeadMenu : Canvas
+    {
+        static HeadMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HeadMenu), new
+            FrameworkPropertyMetadata(typeof(HeadMenu)));
+        }
+
+        public void setIsActiveToFalse()
+        {
+            HeadMenuBtn List = List<HeadMenuBtn>;
+        }
+
+
+
     }
 }
