781dbc3aa57252540ed20c6c9299d0de6c75e60a
sjihdazi <sjihdazi@gmail.com>
2020-06-03 15:47:53 +0200

    views in construction

--- Trollkit/VisualTrolls.xaml.cs
+++ Trollkit/VisualTrolls.xaml.cs
@@ -24,5 +24,10 @@ namespace Trollkit
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
