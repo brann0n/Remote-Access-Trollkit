4ffc710ea9bfdab4f6b44339706464b7672c8a0f
Brandon <43607839+brann0n@users.noreply.github.com>
2020-06-09 11:08:11 +0200

    added the visual handler

---
+++ Trollkit Client/Modules/CommandHandlers/VisualsHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trollkit_Library.Models;
+
+namespace Trollkit_Client.Modules.CommandHandlers
+{
+	public class VisualsHandler : ICommandHandler
+	{
+		public void HandleCommand(TransferCommandObject obj)
+		{
+			switch (obj.Command)
+			{
+				case "BlackScreen":
+				case "TextBox":
+				case "ShowImage":
+				case "OpenSite":
+				case "SetBackground":
+					break;
+			}
+		}
+	}
+}
