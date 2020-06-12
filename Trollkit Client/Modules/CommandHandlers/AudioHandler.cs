d1faa50e3b9df6bc8d2219a9d0bb66dd0290cc82
JarnoHilverts <43601359+JarnoHilverts@users.noreply.github.com>
2020-06-12 09:03:31 +0200

    All mediacontrolls added in code not added the buttons

--- Trollkit Client/Modules/CommandHandlers/AudioHandler.cs
+++ Trollkit Client/Modules/CommandHandlers/AudioHandler.cs
@@ -17,14 +17,12 @@ namespace Trollkit_Client.Modules.CommandHandlers
 		[DllImport("user32.dll")]
 		static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
 
-		private const int KEYEVENTF_EXTENTEDKEY = 1;
-		private const int KEYEVENTF_KEYUP = 0;
 		private const int NEXT_TRACK = 0xB0;
 		private const int PLAY_PAUSE = 0xB3;
 		private const int PREV_TRACK = 0xB1;
-		private const int VOLUME_MUTE = 0x80000;
-		private const int VOLUME_UP = 0xA0000;
-		private const int VOLUME_DOWN = 0x90000;
+		private const int VOLUME_MUTE = 0xAD;
+		private const int VOLUME_UP = 0xAF;
+		private const int VOLUME_DOWN = 0xAE;
 		public bool HandleCommand(TransferCommandObject obj)
 		{
 
@@ -57,27 +55,32 @@ namespace Trollkit_Client.Modules.CommandHandlers
 			}
 			else if (obj.Command == "VolumeUp")
 			{
-				keybd_event((byte)Keys.VolumeUp, 0, 0, IntPtr.Zero);
+				keybd_event(VOLUME_UP, 0, 0, IntPtr.Zero);
 				return true;
 			}
 			else if (obj.Command == "VolumeDown")
 			{
-				keybd_event((byte)Keys.VolumeDown, 0, 0, IntPtr.Zero);
+				keybd_event(VOLUME_DOWN, 0, 0, IntPtr.Zero);
+				return true;
+			}
+			else if (obj.Command == "Mute")
+			{
+				keybd_event(VOLUME_MUTE, 0, 0, IntPtr.Zero);
 				return true;
 			}
 			else if (obj.Command == "Play_Pause")
 			{
-				keybd_event(PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+				keybd_event(PLAY_PAUSE, 0, 1, IntPtr.Zero);
 				return true;
 			}
 			else if (obj.Command == "NextTrack")
 			{
-				keybd_event(NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+				keybd_event(NEXT_TRACK, 0, 1, IntPtr.Zero);
 				return true;
 			}
 			else if (obj.Command == "PreviousTrack")
 			{
-				keybd_event(PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+				keybd_event(PREV_TRACK, 0, 1, IntPtr.Zero);
 				return true;
 			}
 
