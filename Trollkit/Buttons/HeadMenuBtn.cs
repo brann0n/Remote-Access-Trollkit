11c3b2bf5c55897ffdaf32aac3cc5aba87f4b96e
Brandon <43607839+brann0n@users.noreply.github.com>
2020-06-18 15:32:59 +0200

    Fixed menu buttons

--- Trollkit/Buttons/HeadMenuBtn.cs
+++ Trollkit/Buttons/HeadMenuBtn.cs
@@ -1,20 +1,56 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Trollkit.Buttons
 {
 	public class HeadMenuBtn : Button
 	{
+
+		private static HeadMenuBtn focussedBtn;
+		private bool btnDown;
+
 		static HeadMenuBtn()
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(typeof(HeadMenuBtn), new
 			FrameworkPropertyMetadata(typeof(HeadMenuBtn)));
 		}
 
+		protected override void OnMouseLeave(MouseEventArgs e)
+		{			
+			base.OnMouseLeave(e);
+
+			if (btnDown)
+			{
+				if(focussedBtn != null)
+				{
+					focussedBtn.Focus();
+				}
+				else
+				{
+
+				}
+			}
+		}
+
+		protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+		{
+			base.OnMouseLeftButtonDown(e);
+			btnDown = true;
+		}
+
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			base.OnMouseLeftButtonUp(e);
+			btnDown = false;
+		}
+
 		protected override void OnClick()
 		{
 			ContentControl trollView = (ContentControl)this.FindName("trollView");
 			trollView.Content = this.Resources["View"];
+			focussedBtn = this;
+			//Focus();
 		}
 	}
 }
